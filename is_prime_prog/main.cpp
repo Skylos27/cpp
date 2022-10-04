b39e055ad83fc3caa94298e95f41da91b1d86614
Antoine Auger <antoine.auger@epita.fr>
2022-10-04 11:21:23 +0200

    fix is_prime_prog

--- is_prime_prog/main.cpp
+++ is_prime_prog/main.cpp
@@ -1,27 +1,38 @@
 #include <iostream>
 using namespace std;
 
+long long sqrt(long long n){
+    long long i = 0;
+    while(i * i < n)
+        i++;
+    return i; 
+}
+
 int main() {
 
-    int i, n;
+    long long i, n;
     bool is_prime = true;
 
     while(cin >> n){
         bool is_prime = true;
-        if (n == 0 || n == 1) {
+	long long root = sqrt(n);
+        if (n <= 1) {
             is_prime = false;
         }
+	else if (n <= 3){
+	    is_prime = true;
+	}
         else{
-            for (i = 2; i <= n/2; ++i) {
+            for (i = 2; i <= root; ++i) {
                 if (n % i == 0) {
                     is_prime = false;
                 }
             }
         }
         if (is_prime)
-            cout << n << " is a prime : True\n";
+            cout << n << " is a prime: True\n";
         else
-            cout << n << " is not a prime : False\n";
+            cout << n << " is a prime: False\n";
     }
     return 0;
 }
